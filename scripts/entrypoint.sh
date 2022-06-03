4bfdffc1c4eaf57c4c88f70776c709d3d778ac5b
Brad Liang <brad@bliang.com>
2022-06-03 16:54:45 -0700

    cut should handle spaces properly

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ ACSM_PATH=/home/libgourou/files/$ACSM_FILE
 # OUTPUT_FILE=name=$(basename "$ACSM_PATH" .acsm) 
 acsmdownloader --version
 echo ""[util] acsmdownloader -f "$ACSM_PATH"""
-OUTPUT_FILE=$(acsmdownloader -f "$ACSM_PATH" | egrep "epub|pdf" | cut -d " " -f 2)
+OUTPUT_FILE=$(acsmdownloader -f "$ACSM_PATH" | egrep "epub|pdf" | cut -d " " -f 2-)
 echo "      > $OUTPUT_FILE"
 mv $OUTPUT_FILE "output.drm"
 adept_remove -f "output.drm" -o "${ACSM_PATH%.*}.${OUTPUT_FILE##*.}"
